d4b728edad83a140d43fe2d0fb95cc6131ce1280
Ruparajesh <60231638+Ruparajesh@users.noreply.github.com>
2020-03-25 22:17:02 +0530

    Add files via upload

---
+++ RemoveAChar.java
@@ -0,0 +1,14 @@
+package Task6.TDDJ_Task6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RemoveAChar {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
